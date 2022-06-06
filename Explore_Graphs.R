2b5889fc874bc190414236d70184284b4b416ae3
Brachmann <Matthew.Brachmann@dfo-mpo.gc.ca>
2022-06-06 11:56:21 -0300

    Update Explore_Graphs.R

--- Explore_Graphs.R
+++ Explore_Graphs.R
@@ -16,9 +16,24 @@ WG_df = read_csv('WG_df_metadata_cleaned.csv')
 # Map ---------------------------------------------------------------------
 
 
-# Proportion orgin --------------------------------------------------------
+# Proportion origin --------------------------------------------------------
 
+WG_df %>% 
+  group_by(mixture_collection, 
+           origin_id) %>% 
+  summarise(n = n())%>%
+  mutate(freq = n / sum(n)) 
 
 # Change in proportion per time  ------------------------------------------
 
+## need a case_when function to group the 80's and 90's years into
+## two distinct groups... the 80's and 90's
 
+
+WG_df %>% 
+  group_by(year,
+           mixture_collection,
+           origin_id) %>% 
+  summarise(n = n())%>%
+  mutate(freq = n / sum(n)) %>% 
+  View()
