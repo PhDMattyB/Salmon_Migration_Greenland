98210c8d2a6d93ef128dcc57323de414508c3cc4
Brachmann <Matthew.Brachmann@dfo-mpo.gc.ca>
2022-06-06 13:54:43 -0300

    Update Explore_Graphs.R

--- Explore_Graphs.R
+++ Explore_Graphs.R
@@ -13,9 +13,74 @@ library(tidyverse)
 WG_df = read_csv('WG_df_metadata_cleaned.csv')
 
 theme_set(theme_bw())
-# Map ---------------------------------------------------------------------
+# Maps package ---------------------------------------------------------------------
+library(maps)
+library(scatterpie)
+
+pops = map_data('world') %>% 
+  filter(region %in% c('Canada',
+                       'Greenland', 
+                       'Norway', 
+                       'Iceland', 
+                       'UK', 
+                       'state'))%>% 
+  as_tibble()
+
+maine = 
+  map_data('state') %>%
+  filter(region == 'maine') %>% 
+  as_tibble()
+
+mapping_data = bind_rows(pops, 
+                         maine)
 
 
+ggplot(mapping_data) +
+  geom_map(data = mapping_data, 
+           map = mapping_data, 
+           aes(x = long, 
+               y = lat, 
+               map_id = region), 
+           col = 'white', 
+           fill = 'black')+
+  labs(x = 'Longitude', 
+       y = 'Latitude')+
+  # scale_fill_manual(values = map_palette)+
+  theme(axis.title = element_text(size = 14), 
+        axis.text = element_text(size = 12), 
+        legend.title = element_text(size = 14), 
+        legend.text = element_text(size = 12))+
+  geom_scatterpie(data = spread_data, 
+                  aes(x = Longitude, 
+                      y = Latitude, 
+                      group = Population), 
+                  pie_scale = 0.75, 
+                  cols = colnames(spread_data[,c(6:8)]))+
+  coord_fixed()
+
+
+# leaflet package ---------------------------------------------------------
+library(leaflet)
+
+## This plots all rivers where fish were collected
+Sites = WG_df %>%
+  dplyr::select(repunit,
+                River,
+                Lat,
+                Long) %>%
+  as.data.frame()
+
+map = leaflet()
+map = addTiles(map)
+map = addMarkers(map, 
+                 lng = Sites$Long, 
+                 lat = Sites$Lat, 
+                 popup = Sites$repunit)
+
+
+## get an average lat and long for the reporting groups
+
+WG_df
 
 # Change in proportion per time  ------------------------------------------
 
